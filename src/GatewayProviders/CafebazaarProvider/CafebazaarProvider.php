2cb2e9c37a5a43fe91e1a757fc70d0071b219188
PeymanManteali <156663346+PeymanManteali@users.noreply.github.com>
2024-02-13 09:25:05 +0330

    Add files via upload

---
+++ src/GatewayProviders/CafebazaarProvider/CafebazaarProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PaymentService\GatewayProviders\CafebazaarProvider;
+
+use PaymentService\Contracts\GatewayAbstraction;
+use PaymentService\Contracts\GatewayInterface;
+
+class CafebazaarProvider extends GatewayAbstraction implements GatewayInterface
+{
+
+    public function executePayment()
+    {
+        return array('bazaarCode' => $this->subscription->package->bazaar_code);
+    }
+
+    public function confirmPayment($purchaseToken)
+    {
+        $cafebazaar_id = $this->subscription->package->bazaar_code;
+        $cafebazaar = new Cafebazaar();
+        $purchase = $cafebazaar->verifyPurchase($cafebazaar_id, $purchaseToken);
+        if ($purchase->isValid()) {
+            return [
+                'status' => 'success',
+                'time' => $purchase->getTime(),
+                'ref_code' => $purchaseToken,
+            ];
+        }
+        return [
+            'status' => ' failed',
+            'time' => $purchase->getTime(),
+            'ref_code' => $purchaseToken,
+            'description'=>'state: '.$purchase->getPurchaseState(),
+        ];
+    }
+
+    public function refundPayment()
+    {
+        // TODO: Implement refundPayment() method.
+    }
+}
